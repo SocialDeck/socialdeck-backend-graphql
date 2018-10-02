1e0d69b5db405b5275839b992fcef4a3bdb56cdc
Christopher Dean Hagmann <cdhagmann@gmail.com>
2018-10-02 09:19:09 -0400

    Updated Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 require 'faker'
 
 10.times do
-  Address.create(
+  Address.create!(
     address1: Faker::Address.street_address,
     address2: Faker::Address.secondary_address,
     city: Faker::Address.city,
@@ -18,26 +18,59 @@ require 'faker'
 end
 
 10.times do
-  User.create(
+  User.create!(
     username: Faker::Internet.username, 
-    password: Faker::Internet.password(8),
+    password: "user",
     email: Faker::Internet.email,
     number: Faker::PhoneNumber.cell_phone
   )
 end
 
 10.times do
-  Card.create(
+  user = User.find(User.pluck(:id).sample)
+  Card.create!(
     name: Faker::Name.name,
     display_name: Faker::Military.air_force_rank,
     person_name: Faker::Name.name,
-    business_name: Faker::Bank.name,
-    number: Faker::Internet.password(8),
-    email: Faker::Internet.email,
+    business_name: rand < 0.3 ? Faker::Bank.name : nil,
+    number: user.number,
+    email: user.email,
+    user_id: user.id,
+    author_id: user.id,
+    address_id: Address.pluck(:id).sample,
     birth_date: Faker::Date.birthday(18, 65),
-    twitter: Faker::Twitter.user,
-    linked_in: Faker::Name.name,
-    facebook: Faker::Space.galaxy,
-    instagram: Faker::GameOfThrones.house
+    twitter: rand < 0.3 ? "https://twitter.com/" + user.username : nil,
+    linked_in: rand < 0.3 ? Faker::Name.name : nil,
+    facebook: rand < 0.3 ? Faker::Space.galaxy : nil,
+    instagram: rand < 0.3 ? Faker::GameOfThrones.house : nil
+  )
+end
+
+10.times do
+  user = User.find(User.pluck(:id).sample)
+  Card.create!(
+    name: Faker::Name.name,
+    display_name: Faker::Military.air_force_rank,
+    person_name: Faker::Name.name,
+    business_name: rand < 0.3 ? Faker::Bank.name : nil,
+    number: Faker::PhoneNumber.cell_phone,
+    email: Faker::Internet.email,
+    address_id: Address.pluck(:id).sample,
+    author_id: user.id,
+    birth_date: Faker::Date.birthday(18, 65),
+    twitter: rand < 0.3 ? "https://twitter.com/" + user.username : nil,
+    linked_in: rand < 0.3 ? Faker::Name.name : nil,
+    facebook: rand < 0.3 ? Faker::Space.galaxy : nil,
+    instagram: rand < 0.3 ? Faker::GameOfThrones.house : nil
+  )
+end
+
+10.times do
+  user = User.find(User.pluck(:id).sample)
+  card = Card.find(Card.pluck(:id).sample)
+  Connection.create!(
+    user_id:user.id,
+    contact_id: card.user_id,
+    card_id: card.id
   )
 end
