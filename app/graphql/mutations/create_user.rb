cd549b6c526f46230e303ecc774098c2ac501a30
Christopher Dean Hagmann <cdhagmann@gmail.com>
2018-10-02 09:26:21 -0400

    Made signin simpler

--- app/graphql/mutations/create_user.rb
+++ app/graphql/mutations/create_user.rb
@@ -1,21 +1,16 @@
 class Mutations::CreateUser < GraphQL::Function
-  AuthProviderInput = GraphQL::InputObjectType.define do
-    name 'AuthProviderSignupData'
-
-    argument :user, Types::AuthProviderUsernameInput
-  end
-
+  argument :user, !Types::AuthProviderUsernameInput
   argument :email, !types.String
   argument :number, types.String
-  argument :authProvider, !AuthProviderInput
 
   type Types::UserType
 
   def call(_obj, args, _ctx)
     User.create!(
       email: args[:email],
-      username: args[:authProvider][:user][:username],
-      password: args[:authProvider][:user][:password]
+      number: args[:number],
+      username: args[:user][:username],
+      password: args[:user][:password]
     )
   end
 end
