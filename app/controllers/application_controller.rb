f3e9724bc6a8a54578d6f5c00f3dcba8d1f34df0
Christopher Dean Hagmann <cdhagmann@gmail.com>
2018-10-02 09:41:19 -0400

    I hate CSRF

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
   include ActionController::HttpAuthentication::Token::ControllerMethods
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
 end
