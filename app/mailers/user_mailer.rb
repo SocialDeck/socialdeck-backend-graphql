fcdfc8d645e20ec8986a893304c2ec5d4eb45cd7
PhoenixSmith <phoenix.smith.bcbsnc@gmail.com>
2018-10-08 10:15:57 -0400

    Added mailers for signup and password reset

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,32 @@
+class UserMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.signup.subject
+  #
+  def signup(user)
+    @user = user
+    @greeting = "Thanks for signing up! Your username is: #{@user.username}."
+
+    mail(
+      to: @user.email,
+      from: 'info@socialdeck.xyz',
+      subject: 'Welcome to SocialDeck!'
+    )
+
+  end
+
+  def password_reset
+    @user = user
+    @greeting = "Forgot Password?"
+
+    mail(
+      to: @user.email,
+      from: 'info@socialdeck.xyz', 
+      subject: 'Your Password Reset Request'
+    )
+
+  end
+
+end
