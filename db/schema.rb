75f3361833c6239daaa86f55d6359ca856a29bc4
Christopher Dean Hagmann <cdhagmann@gmail.com>
2018-10-09 09:26:23 -0400

    hotfix schema

--- db/schema.rb
+++ db/schema.rb
@@ -77,13 +77,11 @@ ActiveRecord::Schema.define(version: 2018_10_08_210241) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
-    t.string "token"
     t.string "name"
     t.string "email"
     t.boolean "confirmed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["token"], name: "index_users_on_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
