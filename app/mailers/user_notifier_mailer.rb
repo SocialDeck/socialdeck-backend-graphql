a3e37a7a0a7f0b8af93cdba706defce6d79567b8
Christopher Dean Hagmann <cdhagmann@gmail.com>
2018-10-09 11:36:39 -0400

    hotfix email for sending

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,5 +1,5 @@
 class UserNotifierMailer < ApplicationMailer
-    default :from => 'info@socialdeck.com'
+    default :from => 'info@socialdeck.xyz'
   
     # send a signup email to the user, pass in the user object that contains the user's email address
     def send_signup_email(user, token)
